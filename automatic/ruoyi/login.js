18c76542feb967fa3820783761478fadcf73d9d7
3046872685 <67782262+3046872685@users.noreply.github.com>
2020-07-23 15:51:48 +0800

    loginform

--- automatic/ruoyi/login.js
+++ automatic/ruoyi/login.js
@@ -1,47 +1,50 @@
 
-$(function() {
+/*$(function() {
     validateRule();
 	$('.imgcode').click(function() {
 		var url = ctx + "captcha/captchaImage?type=" + captchaType + "&s=" + Math.random();
 		$(".imgcode").attr("src", url);
 	});
-});
-
-$.validator.setDefaults({
+});*/
+/*$.validator.setDefaults({
     submitHandler: function() {
 		login();
     }
+});*/
+$("#btnSubmit").click(function(){
+	login();
 });
-
 function login() {
-	$.modal.loading($("#btnSubmit").data("loading"));
-	var username = $.common.trim($("input[name='username']").val());
-    var password = $.common.trim($("input[name='password']").val());
-    var validateCode = $("input[name='validateCode']").val();
-    var rememberMe = $("input[name='rememberme']").is(':checked');
+	/*$.modal.loading($("#btnSubmit").data("loading"));*/
+	var username = $("input[name='username']").val();
+    var password = $("input[name='password']").val();
+ /*   var validateCode = $("input[name='validateCode']").val();*/
+  /*  var rememberMe = $("input[name='rememberme']").is(':checked');*/
     $.ajax({
         type: "post",
-        url: ctx + "login",
+        url: "http://localhost:8888/LoginForm/loginFind",
         data: {
-            "username": username,
+            "loginname": username,
             "password": password,
-            "validateCode" : validateCode,
-            "rememberMe": rememberMe
+            /*"validateCode" : validateCode,*/
+            /*"rememberMe": rememberMe*/
         },
-        success: function(r) {
-            if (r.code == 0) {
-                location.href = ctx + 'index';
-            } else {
-            	$.modal.closeLoading();
-            	$('.imgcode').click();
-            	$(".code").val("");
-            	$.modal.msg(r.msg);
+        success: function(res) {
+            if(res.cote=="10001"){
+            	alert("无此用户");
+            }else if(res.cote=="10002"){
+            	alert("密码错误");
+            }else if(res.cote=="10000"){
+            	console.log(res.t);
+            	sessionStorage.setItem("userObject",JSON.stringify(res.t));
+            	window.location.href="index.html";
             }
         }
     });
 }
 
-function validateRule() {
+
+/*function validateRule() {
     var icon = "<i class='fa fa-times-circle'></i> ";
     $("#signupForm").validate({
         rules: {
@@ -61,4 +64,4 @@ function validateRule() {
             }
         }
     })
-}
+}*/
